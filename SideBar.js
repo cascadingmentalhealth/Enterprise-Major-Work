34899f858a3e994adcb4348369b02384f8385d56
samiqa_rashid <samiqa.rashid@education.nsw.gov.au>
2025-06-01 20:22:08 +1000

    sidebar

--- SideBar.js
+++ SideBar.js
@@ -1,1 +1,6 @@
-s
+const resizeBtn = document.querySelector('[data-resize-btn');
+
+resizeBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sb-expanded')
+});
